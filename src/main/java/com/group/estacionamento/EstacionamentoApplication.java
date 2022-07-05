161f99f9c39a74ccb7c6c232d68dcfda4ca61e88
Italo <italo.augusto.ct@gmail.com>
2022-07-05 20:02:44 -0300

    Created the project

---
+++ src/main/java/com/group/estacionamento/EstacionamentoApplication.java
@@ -0,0 +1,13 @@
+package com.group.estacionamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionamentoApplication.class, args);
+	}
+
+}
